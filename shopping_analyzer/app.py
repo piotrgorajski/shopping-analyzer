1d3ba70439f4ff8d7bbba758687d1d162eda45f7
Your Name <you@example.com>
2020-05-22 12:57:13 +0200

    Add logging facility

--- shopping_analyzer/app.py
+++ shopping_analyzer/app.py
@@ -1,14 +1,23 @@
+import logging
+
 from shopping_analyzer.argument_parser import get_app_arguments
 from shopping_analyzer.receipt_processor.receipt_processor import process_input_receipts
 from shopping_analyzer.shopping_statistics.shopping_statistics import generate_statistics
 
 
 def run():
+    # set up logging
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s %(message)s')
+    logging.info('Application Start')
+
     # read app arguments
     app_args = get_app_arguments()
 
     # ocr and process source files
     receipts = process_input_receipts(app_args.source, app_args.perform_ocr)
+    logging.info(f'Processing {len(receipts)} receipts')
 
     # generate various statistics as csv files
     generate_statistics(receipts, app_args.source)
+
+    logging.info('Application End')
