db6a50e8172b80fd0f87f4e3bdb7120c947524f6
Your Name <you@example.com>
2020-05-25 16:28:19 +0200

    Add logging for invalid single receipt item

--- shopping_analyzer/receipt_processor/receipt_text_processor.py
+++ shopping_analyzer/receipt_processor/receipt_text_processor.py
@@ -5,6 +5,9 @@ from functional import seq
 
 from shopping_analyzer.receipt_processor.receipt import ReceiptItem, Receipt
 
+failed_receipts_count = 0
+failed_receipt_items_count = 0
+
 receipt_pattern = re.compile(
     r".*www\.lidl\.pl\s*(?P<date>\d{4}-\d{2}-\d{2})(?P<items>.*)PTU A.*RAZEM PLN\s+(?P<total_amount>\d+,\d{2}).*",
     re.DOTALL)
@@ -19,7 +22,6 @@ def process_ocr_receipts(ocr_receipts):
 
 
 def process_single_text_receipt(ocr_receipt):
-    # TODO Pass here also receipt metadata to be able to log invalid receipt filename
     receipt_match = re.match(receipt_pattern, ocr_receipt.receipt_text)
     if receipt_match:
         date = receipt_match.group('date')
@@ -30,12 +32,15 @@ def process_single_text_receipt(ocr_receipt):
         # TODO Log as error any issues in the data
         return Receipt(date, receipt_items, total_amount)
     else:
-        logging.error(f'Failed to match whole receipt: [{ocr_receipt.source_file_name}]')
+        global failed_receipts_count
+        failed_receipts_count += 1
+        logging.error(f'{failed_receipts_count} Failed to match whole receipt: [{ocr_receipt.source_file_name}]')
 
 
 def extract_receipt_items(text_receipt):
     text_receipt_item_lines = text_receipt.splitlines()
     return seq(text_receipt_item_lines) \
+        .filter(None) \
         .map(lambda text_receipt_item: parse_single_receipt_item(text_receipt_item)) \
         .filter(None) \
         .to_list()
@@ -46,6 +51,10 @@ def parse_single_receipt_item(text_receipt_item):
     # TODO Add more complex filtering (lidl discounts)
     if item_match:
         return parse_matching_text_receipt_item(item_match)
+    else:
+        global failed_receipt_items_count
+        failed_receipt_items_count += 1
+        logging.error(f'{failed_receipt_items_count} Failed to process following receipt item: {text_receipt_item}')
 
 
 def parse_matching_text_receipt_item(item_match):
