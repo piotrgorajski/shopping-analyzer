d401ebba7634cd3ca7bf9b82dae526c646cdb8bd
Your Name <you@example.com>
2020-05-28 15:32:57 +0200

    Enhance receipt item patterns

--- shopping_analyzer/receipt_processor/receipt_text_processor.py
+++ shopping_analyzer/receipt_processor/receipt_text_processor.py
@@ -6,6 +6,7 @@ from functional import seq
 from shopping_analyzer.receipt_processor.receipt import ReceiptItem, Receipt
 
 failed_receipts_count = 0
+all_receipt_items_count = 0
 failed_receipt_items_count = 0
 
 receipt_pattern = re.compile(
@@ -13,11 +14,11 @@ receipt_pattern = re.compile(
     re.DOTALL)
 
 regular_item_pattern = re.compile(
-    r"(?P<name>.+)\s+(?P<quantity>\d+)\s*\*\s+(?P<price>\d+,\d{2})\s+(?P<cost>\d+,\d{2})\s*.*")
+    r"(?P<name>.+)\s+(?P<quantity>\d+(,\s*\d{2,3})?)\s*[* ]\s*(?P<price>\d+,\s*\d{2})\s+(?P<cost>\d+,\s*\d{2})\s*.*")
 
-lidl_plus_discount_item_pattern = re.compile(r"Lidl Plus rabat\s+-(?P<discount_amount>\d+,\d{2})")
+lidl_plus_discount_item_pattern = re.compile(r"Lidl Plus rabat\s+-(?P<discount_amount>\d+,\s*\d{2})")
 
-common_discount_item_pattern = re.compile(r"^(?!Lidl Plus rabat).*\s+-(?P<discount_amount>\d+,\d{2})")
+common_discount_item_pattern = re.compile(r"^(?!Lidl Plus rabat).*\s+-(?P<discount_amount>\d+,\s*\d{2})")
 
 
 def process_ocr_receipts(ocr_receipts):
@@ -34,25 +35,31 @@ def process_single_text_receipt(ocr_receipt):
         total_amount = receipt_match.group('total_amount')
         raw_receipt_items = receipt_match.group('items')
         receipt_items = extract_receipt_items(raw_receipt_items)
+        # logging.info(f"Count of all receipt items is: {all_receipt_items_count}")
         # TODO Perform validation on items sum vs total amount
         # TODO Log as error any issues in the data
         return Receipt(date, receipt_items, total_amount)
     else:
         global failed_receipts_count
         failed_receipts_count += 1
-        logging.error(f'{failed_receipts_count} Failed to match whole receipt: [{ocr_receipt.source_file_name}]')
+        # logging.error(f'{failed_receipts_count} Failed to match whole receipt: [{ocr_receipt.source_file_name}]')
 
 
 def extract_receipt_items(text_receipt):
     text_receipt_item_lines = text_receipt.splitlines()
     return seq(text_receipt_item_lines) \
         .filter(None) \
+        .map(lambda text_receipt_item: clean_text_receipt_item(text_receipt_item)) \
         .map(lambda text_receipt_item: parse_single_receipt_item(text_receipt_item)) \
         .filter(None) \
         .to_list()
 
 
 def parse_single_receipt_item(text_receipt_item):
+    # TODO Handle such counts somehow nicer
+    global all_receipt_items_count
+    all_receipt_items_count += 1
+
     regular_item_match = re.match(regular_item_pattern, text_receipt_item)
     lidl_plus_discount_item_match = re.match(lidl_plus_discount_item_pattern, text_receipt_item)
     common_discount_item_match = re.match(common_discount_item_pattern, text_receipt_item)
@@ -68,20 +75,27 @@ def parse_single_receipt_item(text_receipt_item):
         logging.error(f'{failed_receipt_items_count} Failed to process following receipt item: {text_receipt_item}')
 
 
+def clean_text_receipt_item(text_receipt_item):
+    return text_receipt_item \
+        .replace('%', '') \
+        .replace('\\', '') \
+        .replace('/', '')
+
+
 def parse_matching_regular_item(item_match):
     # TODO Perform validation on items like if the a * b = c
     name = item_match.group('name')
-    quantity = item_match.group('quantity')
-    price = item_match.group('price')
-    cost = item_match.group('cost')
+    quantity = item_match.group('quantity').replace(' ', '')
+    price = item_match.group('price').replace(' ', '')
+    cost = item_match.group('cost').replace(' ', '')
     return ReceiptItem(name, quantity, price, cost)
 
 
 def parse_matching_lidl_plus_discount_item(item_match):
-    cost = item_match.group('discount_amount')
+    cost = item_match.group('discount_amount').replace(' ', '')
     return ReceiptItem('Lidl Plus rabat', None, None, cost)
 
 
 def parse_matching_common_discount_item(item_match):
-    cost = item_match.group('discount_amount')
+    cost = item_match.group('discount_amount').replace(' ', '')
     return ReceiptItem('standardowy rabat', None, None, cost)
